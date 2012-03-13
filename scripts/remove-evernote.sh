627a2d95fe51cd8b106626ae2189b2fe5632c521
Daniel Hjerth <d.hjerth@gmail.com>
2012-03-13 15:09:36 +0100

    asd

---
+++ scripts/remove-evernote.sh
@@ -0,0 +1,8 @@
+killall Safari
+killall Evernote
+rm -rf /Applications/Evernote.app
+rm "./Library/Internet Plug-Ins/EvernoteSafariClipperPlugin.webplugin/Contents/MacOS/EvernoteSafariClipperPlugin"
+rm "./Library/Internet Plug-Ins/EvernoteSafariClipperPlugin.webplugin"
+rm "./Library/Application Support/Growl/Tickets/Evernote.growlTicket"
+rm "./Library/Application Support/Evernote/data/*/Evernote.sql"
+rm "./Library/Application Support/Evernote"
